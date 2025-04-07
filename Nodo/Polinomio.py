98babf78567c6eca5eabd9b03c6e86618801017c
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-07 09:51:46 +0000

    Se añadio las funciones sumar y multiplicar

--- Nodo/Polinomio.py
+++ Nodo/Polinomio.py
@@ -56,11 +56,12 @@ class Polinomio:
             if termino == self.grado:
                 self.grado = self.termino_mayor.dato.termino if self.termino_mayor else -1
 
-    def existe_termino(self, termino):
-        """Determina si un término existe en el polinomio."""
+    def modificar_termino(self, termino, nuevo_valor):
+        """Modifica el valor de un término existente en el polinomio."""
         actual = self.termino_mayor
         while actual is not None:
             if actual.dato.termino == termino:
+                actual.dato.valor = nuevo_valor
                 return True
             actual = actual.siguiente
         return False
@@ -70,6 +71,39 @@ class Polinomio:
         actual = self.termino_mayor
         polinomio_str = ""
         while actual is not None:
-            polinomio_str += f"{actual.dato.valor}x^{actual.dato.termino} "
+            signo = " + " if actual.dato.valor > 0 and polinomio_str else ""
+            polinomio_str += f"{signo}{actual.dato.valor}x^{actual.dato.termino}"
             actual = actual.siguiente
         print(polinomio_str.strip())
+
+    def sumar(self, otro_polinomio):
+        """Suma el polinomio actual con otro polinomio."""
+        resultado = Polinomio()
+        actual = self.termino_mayor
+
+        while actual is not None:
+            resultado.agregar_termino(actual.dato.valor, actual.dato.termino)
+            actual = actual.siguiente
+
+        otro_actual = otro_polinomio.termino_mayor
+        while otro_actual is not None:
+            resultado.agregar_termino(otro_actual.dato.valor, otro_actual.dato.termino)
+            otro_actual = otro_actual.siguiente
+
+        return resultado
+
+    def multiplicar(self, otro_polinomio):
+        """Multiplica el polinomio actual con otro polinomio."""
+        resultado = Polinomio()
+        actual = self.termino_mayor
+
+        while actual is not None:
+            otro_actual = otro_polinomio.termino_mayor
+            while otro_actual is not None:
+                nuevo_valor = actual.dato.valor * otro_actual.dato.valor
+                nuevo_termino = actual.dato.termino + otro_actual.dato.termino
+                resultado.agregar_termino(nuevo_valor, nuevo_termino)
+                otro_actual = otro_actual.siguiente
+            actual = actual.siguiente
+
+        return resultado
