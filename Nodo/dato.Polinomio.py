15c8068f2c70943729f66b97b585e15cdb8950a8
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-07 09:35:07 +0000

    Se creo la clase datoPolinomio

---
+++ Nodo/dato.Polinomio.py
@@ -0,0 +1,14 @@
+class datoPolinomio(object):
+    """
+    Clase que representa un polinomio.
+    """
+
+    def __init__(self, valor, termino):
+        """
+        Inicializa el polinomio con un valor y un término.
+        :param valor: Valor del polinomio.
+        :param termino: Término del polinomio.
+        """
+        self.valor = valor
+        self.termino = termino
+   
