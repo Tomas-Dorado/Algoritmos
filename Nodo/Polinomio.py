860a0a42d0cb0c1e44ca2d88275630d43af60ba6
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-07 09:40:31 +0000

    Se creo main y lanzador

---
+++ Nodo/Polinomio.py
@@ -0,0 +1,9 @@
+class Polinomio(object):
+    """Clase Polinomio"""
+    def __init__(self):
+        self.termino_mayor= None
+        self.grado-1
+
+    def agregar_termino(self, coeficiente, exponente):
+        """Agrega un término al polinomio"""
+        aux= Nodo()
